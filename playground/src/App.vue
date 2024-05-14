c3e83cb96caf1b282335d79437f520bc74e9735b
CofCat456 <a25690756@gmail.com>
2024-05-14 17:21:58 +0800

    feat: basic poc

---
+++ playground/src/App.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { h, onMounted, shallowRef } from 'vue'
+
+import { PDFPrinter } from '../../src/index'
+import Com from './components/Com.vue'
+
+const PDF = shallowRef<PDFPrinter | undefined>()
+
+onMounted(() => {
+  PDF.value = new PDFPrinter(
+    h(Com, { title: 'test pdf' }),
+    {
+      title: 'test pdf',
+    },
+  )
+})
+</script>
+
+<template>
+  <main w100vw h100dvh font-sans grid="~ place-content-center" p="x-4 y-10" text="center gray-700 dark:gray-200">
+    <template v-if="PDF">
+      <div flex="~ justify-center items-center gap2">
+        <button class="btn" @click="PDF.preview()">
+          preview
+        </button>
+        <button class="btn" @click="PDF.print()">
+          print
+        </button>
+      </div>
+    </template>
+  </main>
+</template>
---
+++ playground/src/components/Com.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineProps<{
+  title: string
+}>()
+</script>
+
+<template>
+  <section w100vw h100dvh font-sans grid="~ place-content-center" p="x-4 y-10" text="center gray-700 dark:gray-200">
+    <p mb10 text-indigo text-3xl>
+      {{ title }}
+    </p>
+
+    <p fixed top-0>
+      fixed on top0
+    </p>
+    <p fixed bottom-0>
+      fixed on bottom0
+    </p>
+  </section>
+</template>
